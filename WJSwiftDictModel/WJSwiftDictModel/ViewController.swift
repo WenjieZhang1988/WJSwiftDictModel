61567d370110c978e18073e949b5c575c0c16476
Kevin <Kevin.1988@foxmail.com>
2015-03-24 08:36:40 +0800

    Swift 简易字典转模型

---
+++ WJSwiftDictModel/WJSwiftDictModel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WJSwiftDictModel
+//
+//  Created by Kevin on 15/3/24.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
